037f0ac89a95bcb4151df28a55efdb656e0bc8f9
杨北锋 <yangbf@bosera.com>
2020-04-20 18:40:44 +0800

    text file

--- data_wind.py
+++ data_wind.py
@@ -1,1 +1,6 @@
-import WindPy
+
+import WindPy  as wp
+import pandas as pd
+
+data = wp.w.wsd("110011.OF", "nav,NAV_adjfactor,NAV_acc,peer_fund_return_rank_per", "2020-03-21", "2020-04-19", "fundType=1")
+data_df = pd.DataFrame()
